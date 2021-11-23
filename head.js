05de4ab525b7032ba4ec36039da6a8c48960806f
Amal Botan <amalbotan254@gmail.com>
2021-11-23 00:32:38 +0000

    Linted

--- head.js
+++ head.js
@@ -12,9 +12,9 @@ assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 
 
-const head = function(arr){
-  return(arr[0]);
-}
+const head = function(arr) {
+  return (arr[0]);
+};
 
 assertEqual(head([5,2,3,4]), 5);
 assertEqual(head([]), 5);
