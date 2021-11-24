d5fb4615987280fe9f2b4be2695b9c30558e8c24
Amal Botan <amalbotan254@gmail.com>
2021-11-24 01:18:15 +0000

    Created function to compare arrays

---
+++ eqArrays.js
@@ -0,0 +1,33 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+//eqArray Function
+
+const eqArrays = function(arr1, arr2){
+  let result = true;
+  if(arr1.length === arr2.length){
+    for(let i = 0; i < arr1.length; i++){
+      if(arr1[i] !== arr2[i]){
+        result = false;
+        break;
+      }
+    }
+  } 
+  return result;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
