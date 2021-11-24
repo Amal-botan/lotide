b4e51b3e9ccbd104ab19f957fbd4b0533ffa9e71
Amal Botan <amalbotan254@gmail.com>
2021-11-24 05:25:21 +0000

    Linted

--- eqArrays.js
+++ eqArrays.js
@@ -13,21 +13,22 @@ assertEqual(1, 1);
 
 //eqArray Function
 
-const eqArrays = function(arr1, arr2){
+const eqArrays = function(arr1, arr2) {
   let result = true;
-  if(arr1.length === arr2.length){
-    for(let i = 0; i < arr1.length; i++){
-      if(arr1[i] !== arr2[i]){
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
         result = false;
         break;
       }
     }
-  } 
+  }
   return result;
 };
+
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
