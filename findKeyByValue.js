1e79316f911281cbb8a47c1dc784cd116e50980a
Amal Botan <amalbotan254@gmail.com>
2021-11-25 01:51:07 +0000

    Completed findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  //It should scan the object and return the first key which contains the given value.
+  let result;
+  for (const key in object) {
+    if (object[key] === value) {
+      result = key;
+    }
+  }
+  return result;
+};
+
+
+
+//Testing
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
